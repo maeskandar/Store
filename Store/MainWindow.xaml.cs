8eeb15f57b117bbd5cf1790a9bbe0d5d73514b52
Zhino Group <Zhino Group@DESKTOP-ST2SL3K>
2019-06-27 05:23:57 -0700

    پروژه رو ساختم صفحه ی لاگین تستی هم زدم

---
+++ Store/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Store
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void LoginClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("وارد شدید.");
+            var client = new HttpClient();
+            var response = await client.GetStringAsync("http://localhost:51790/api/values");
+        }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow login = new LoginWindow();
+            login.Show();
+            this.Hide();
+        }
+    }
+}
